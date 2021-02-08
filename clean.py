f43aefa7f88706e574df0ca36ec34a1a99121a82
dotX12 <hash@shitposting.team>
2021-02-08 22:08:56 +0300

    Reformat clean.py

--- clean.py
+++ clean.py
@@ -1,15 +1,14 @@
-f = open("mcpi.dump")
-ass = f.read().split('\n')
-f.close()
-ass1 = []
-n = 0
-for i in ass:
-    ii = i.split(" ")
-    if "<UNDEFINED>" in i or "..." in i or len(ii) < 8:
-        continue
-    lenth = len(ii)
-    _car = 0
-    _temp = ' '.join(ii[-(lenth-4):])
-    print(ii)
-    ass1.append(_temp)
-    n += 1
+output = []
+
+with open('mcpi.dump') as f:
+    dump = f.read().split('\n')
+    for i in dump:
+        i_split = i.split()
+        if "<UNDEFINED>" in i_split or i_split == ['...']:
+            continue
+        i_formatted = [j.replace('\t', '') for j in i_split if j not in ['', ';']]
+        only_mnemonic = i_formatted[2:]
+        print(only_mnemonic)
+        output.append(only_mnemonic)
+
+print(output)
